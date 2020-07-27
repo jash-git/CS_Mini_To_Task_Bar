21eec85ab3d09e6fd7c8f336d382d797da6fdafe
Gitea <gitea@fake.local>
2020-07-27 15:49:59 +0800

    Create

---
+++ Mini_To_Task_Bar/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini_To_Task_Bar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //縮小至工作列
+            this.notifyIcon1.Text = "Aria2 Control Panel 已縮小至工作列";
+            this.notifyIcon1.Icon = Properties.Resources.Panel;
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //在工具列點擊兩下，回復視窗
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                this.notifyIcon1.Visible = false;
+            }
+        }
+
+        private void ToolStripMenuItem_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,11 @@
+C# WinForm畫面大小控制[CS_Mini_To_Task_Bar] 
+
+資料來源: https://wijtb.nctu.me/archives/260/
+
+
+單純畫面最大/最小化
+this.WindowState= FormWindowState.Minimized;
+this.WindowState = FormWindowState.Maximized;
+this.WindowState = FormWindowState.Normal;
+
+縮小至工具列 [https://wijtb.nctu.me/archives/260/]
